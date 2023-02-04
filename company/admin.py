ff3b4e6f03d12997c7315a57e52271fd46b8907e
1mac11 <9685440zar>
2023-02-04 11:03:31 +0500

    Removed unnecesssary model from admin page and move send mail process to another file

--- company/admin.py
+++ company/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.utils.safestring import mark_safe
+from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import TokenProxy
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 
-from .models import Company, Location, LocationImages
+from .models import Company
 
 
 class CompanyAdmin(admin.ModelAdmin):
@@ -13,42 +15,8 @@ class CompanyAdmin(admin.ModelAdmin):
     autocomplete_fields = ('owner',)
 
 
-class LocationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'address', 'company', 'company_owner')
-    ordering = ['id']
-    list_display_links = ('id', 'name')
-    search_fields = ('name', 'address')
-    list_filter = ('company',)
-    filter_horizontal = ('employee',)
-    autocomplete_fields = ('company',)
-
-    def company_owner(self, obj):
-        return f'{obj.company.owner} id={obj.company.owner.id}'
-
-
-class LocationImagesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'location', 'location_company', 'location_company_owner', 'get_html_photo')
-    ordering = ['id']
-    list_display_links = ('id', 'title')
-    search_fields = ('name', 'location')
-    list_filter = ('location',)
-    readonly_fields = ('get_html_photo',)
-    autocomplete_fields = ('location',)
-
-    def get_html_photo(self, obj):
-        if obj.image:
-            return mark_safe(f'<img src = "{obj.image.url}" width = "300"/>')
-
-    get_html_photo.short_description = 'image'
-    get_html_photo.allow_tags = True
-
-    def location_company(self, obj):
-        return f'{obj.location.company} id={obj.location.company.id}'
-
-    def location_company_owner(self, obj):
-        return f'{obj.location.company.owner} id={obj.location.company.owner.id}'
-
-
 admin.site.register(Company, CompanyAdmin)
-admin.site.register(Location, LocationAdmin)
-admin.site.register(LocationImages, LocationImagesAdmin)
+admin.site.unregister(Group)
+admin.site.unregister(TokenProxy)
+admin.site.unregister(BlacklistedToken)
+admin.site.unregister(OutstandingToken)
