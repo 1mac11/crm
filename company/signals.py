0d5fd1e541374d3f4725142087735e97cd7e2fdc
Rajabboy757 <rajrajrajradju@gmail.com>
2023-04-03 10:35:52 +0500

    add sending Product changes to telegram

--- company/signals.py
+++ company/signals.py
@@ -1,20 +1,25 @@
 from asgiref.sync import async_to_sync, sync_to_async
+from asyncio.log import logger
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from .alerter import Alerter
 
 from .models import Product
 from channels.layers import get_channel_layer
 
 
-# @receiver(pre_save, sender=Product)
-# def send_message(sender, instance, **kwargs):
-#     print(Product.objects.get(pk=instance.pk))
-
 @receiver(post_save, sender=Product)
 def send_message(sender, instance, created, **kwargs):
-    action = 'created' if created else 'updated'
+    try:
+        action = 'created' if created else 'updated'
+        text = f'{instance} {action}'
 
-    async_to_sync(get_channel_layer().group_send)(
-        f"{instance.company.owner.id}-notifications", {"type": "send_last_message",
-                                                       "text": f'{instance} {action}'}
-    )
+        async_to_sync(get_channel_layer().group_send)(
+            f"{instance.company.owner.id}-notifications", {"type": "send_last_message", "text": text}
+        )
+
+        tg_alert = Alerter.from_environment()
+        tg_alert.custom_send_message(text)
+
+    except Exception as e:
+        logger.error(e)
