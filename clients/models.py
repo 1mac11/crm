7d0582dddafec773b88a5bf5127a7d7c35db301c
Rajabboy757 <rajrajrajradju@gmail.com>
2023-05-27 22:32:11 +0500

    created model for new clients

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from company.models import Product, Company
 
-# Create your models here.
+
+class Clients(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    father_name = models.CharField(max_length=20, blank=True, null=True)
+    bought_products = models.ManyToManyField(Product, blank=True, null=True, related_name='bought_products')
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField()
