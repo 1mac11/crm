ff2f412438c0efb6d89b5dfba574da8678cfd01a
Rajabboy757 <rajrajrajradju@gmail.com>
2023-06-10 17:46:07 +0500

    Removed some unnecessary parts of code

--- clients/urls.py
+++ clients/urls.py
@@ -7,7 +7,5 @@ router = routers.DefaultRouter()
 router.register(r'', views.ClientsViewSet)
 
 urlpatterns = [
-    path('myclients/', views.MyClientsListView.as_view(), name='myclients_all'),
-    path('clients_list/', views.FilteredClientsList.as_view(), name='clients_list_filtered'),
     path('', include(router.urls)),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,10 +1,8 @@
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet
-from rest_framework.response import Response
 from .models import Clients
 from clients import serializers
-from rest_framework import generics
 
 
 class ClientFilter(FilterSet):
@@ -26,20 +24,4 @@ class ClientsViewSet(viewsets.ModelViewSet):
     filterset_class = ClientFilter
     http_method_names = ('get', 'post', 'patch', 'delete')
 
-# class MyClientsListView(generics.GenericAPIView):
-#     serializer_class = serializers.MyClientsSerializer
-#
-#     def post(self, request):
-#         company_id = request.data.get('company_id')
-#         # below we get the clients by comparing their bought_products' company ids with our company id
-#         clients = Clients.objects.filter(bought_products__company_id=company_id)
-#         serializer = serializers.ClientSerializer(clients, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#
-# class FilteredClientsList(generics.ListAPIView):
-#     queryset = Clients.objects.all()
-#     serializer_class = serializers.ClientSerializer
-#     filterset_class = ClientFilter
-#     # filter_backends = [DjangoFilterBackend]
-#     # filterset_fields = ['name', 'surname', 'phone', 'email']
+
