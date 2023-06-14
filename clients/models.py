102c6fb5642682a73d4db032e9465ad553911386
Rajabboy757 <rajrajrajradju@gmail.com>
2023-06-14 21:59:12 +0500

    add str representing

--- clients/models.py
+++ clients/models.py
@@ -10,3 +10,6 @@ class Clients(models.Model):
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.name + self.surname
