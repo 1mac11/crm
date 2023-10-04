40b4e63c32bcc137afc625545fa89fdbb90ffd0a
Rajabboy757 <rajrajrajradju@gmail.com>
2023-10-04 09:20:36 +0500

    corrected some issues in celery.py and tasks.py

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'redismail.settings')
 
@@ -19,7 +20,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'send-ad-mail-every-day': {
         'task': 'myapp2.tasks.send_ad_mails',
-        'schedule': crontab(hour=15, minute=56),
+        'schedule': crontab(hour='15', minute='56'),
         'args' : ("I am a Nigerian Prince.",)
     }
 }
